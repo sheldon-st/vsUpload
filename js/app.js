b51d78be6b8fdf02feebabf58c032353e0a5532b
sheldon-st <sheldon.st@northeastern.edu>
2021-04-14 14:29:05 -0400

    animation

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
+anime.timeline({ loop: false }).add({
+  targets: ".doodle-container .doodlefont",
+  scale: [4, 1],
+  opacity: [0, 1],
+  easing: "easeOutCirc",
+  duration: 100,
+  delay: (el, i) => 50 * i,
+});
+
 new fullpage("#Artish", {
   autoScrolling: true,
   navigation: false,
