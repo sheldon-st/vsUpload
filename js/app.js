bcc2a34fbf8edbb3cca0b8b02f2d8c35a4866727
sheldon-st <sheldon.st@northeastern.edu>
2021-04-21 11:35:30 -0400

    updates

--- js/app.js
+++ js/app.js
@@ -1,18 +1,12 @@
-anime.timeline({ loop: false }).add({
-  targets: ".doodle-container .doodlefont",
-  scale: [0.2, 1],
-  opacity: [0, 1],
-  easing: "easeOutCirc",
-  duration: 200,
-  delay: (el, i) => 100 * i,
-});
-
 new fullpage("#Artish", {
   autoScrolling: true,
   navigation: false,
 });
 new fullpage("#fullpage", {
   autoScrolling: true,
-  navigation: true,
+  navigation: false,
   navigationPosition: "left",
+  scrollHorizontally: true,
+  slidesNavigation: true,
+  slidesNavPosition: "bottom",
 });
