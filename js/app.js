1898c8dbc60d26309ef832072985968d3174acdf
sheldon-st <sheldon.st@northeastern.edu>
2021-04-24 18:20:19 -0400

    updates

--- js/app.js
+++ js/app.js
@@ -8,5 +8,16 @@ new fullpage("#fullpage", {
   navigationPosition: "left",
   scrollHorizontally: true,
   slidesNavigation: true,
+
   slidesNavPosition: "bottom",
+  navigationTooltips: [
+    "assignment 1",
+    "assignment 2",
+    "assignment 3",
+    "assignment 4",
+    "assignment 5",
+    "assignment 6",
+    "assignment 7",
+  ],
+  showActiveTooltip: true,
 });
