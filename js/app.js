9f64a52cf14920583108f3f6b8b52c13ed3f9ee4
sheldon-st <sheldon.st@northeastern.edu>
2021-04-28 23:51:05 -0400

    testing for animations

--- js/app.js
+++ js/app.js
@@ -1,25 +1,65 @@
-new fullpage("#Artish", {
-  autoScrolling: true,
-  navigation: false,
-});
 new fullpage("#fullpage", {
   autoScrolling: true,
-  navigation: true,
+  navigation: false,
   navigationPosition: "left",
   scrollHorizontally: true,
   slidesNavigation: true,
 
   slidesNavPosition: "bottom",
-  navigationTooltips: [
-    "ui/ux",
-    "assignment 2",
-    "assignment 3",
-    "assignment 4",
-    "assignment 5",
-    "assignment 6",
-    "assignment 7",
-  ],
+  navigationTooltips: ["ui/ux", "assignment 2", "assignment 3"],
   showActiveTooltip: true,
   continuousHorizontal: true,
-  controlArrows: false,
+  controlArrows: true,
 });
+
+// tell Barba to use the prefetch plugin
+/* 
+barba.use(barbaPrefetch);
+
+barba.init({
+  views: [
+    {
+      namespace: "home",
+      beforeEnter() {
+        // update the menu based on user navigation
+        // menu.update();
+        fullpage_api.destroy("all");
+        // fullpage_api.destroy();
+        fullpage_api.reBuild();
+      },
+      afterEnter() {
+        fullpage_api.reBuild();
+        // refresh the parallax based on new page content
+      },
+    },
+    {
+      namespace: "work",
+      beforeEnter() {
+        // update the menu based on user navigation
+        AddFullpage();
+        barba.prefetch(URL);
+      },
+      afterEnter() {
+        // refresh the parallax based on new page content
+        fullpage_api.reBuild();
+      },
+    },
+  ],
+
+  transitions: [
+    {
+      name: "opacity-transition",
+      leave(data) {
+        return gsap.to(data.current.container, {
+          opacity: 0,
+        });
+      },
+      enter(data) {
+        return gsap.from(data.next.container, {
+          opacity: 0,
+        });
+      },
+    },
+  ],
+});
+ */
--- node_modules/@haiku/core/dom/angular/index.js
+++
@@ -1,5 +0,0 @@
-/**
- * Copyright (c) Haiku 2016-2018. All rights reserved.
- */
-
-module.exports = require('../../lib/adapters/angular-dom/HaikuAngularDOMAdapter').default;
