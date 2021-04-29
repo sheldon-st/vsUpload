322abc4d233a6e11b3a436a875b32f2882e822f6
sheldon-st <sheldon.st@northeastern.edu>
2021-04-29 08:48:34 -0400

    menu update (ignore)

--- js/app2.js
+++
@@ -1,8 +0,0 @@
-anime.timeline({ loop: false }).add({
-  targets: ".doodle-container .doodlefont",
-  scale: [4, 1],
-  opacity: [0, 1],
-  easing: "easeOutCirc",
-  duration: 100,
-  delay: (el, i) => 50 * i,
-});
