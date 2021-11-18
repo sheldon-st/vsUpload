277e6c1118c79787d290d9d7a49e643d75d0eb2b
sheldon-st <sheldon.st@northeastern.edu>
2021-11-18 09:19:46 -0500

    link update

--- src/components/social/social.js
+++ src/components/social/social.js
@@ -4,18 +4,24 @@ import { NavLink } from "react-router-dom";
 import logo from "../../img/1x/logoImg.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithubAlt } from "@fortawesome/free-brands-svg-icons";
+import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
+import { faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
 
 const Social = () => {
   return (
     <div class="social-btns">
-      <a class="btn email" href="#">
-        <FontAwesomeIcon className="fa" icon={faGithubAlt} />
+      <a class="btn email" href="mailto:sheldon.st@northeastern.edu">
+        <FontAwesomeIcon className="fa" icon={faPaperPlane} color="E07A5F" />
       </a>
-      <a class="btn linkedin" href="#">
-        <FontAwesomeIcon className="fa" icon={faGithubAlt} />
+      <a class="btn linkedin" href="https://www.linkedin.com/in/stephen-sheldon">
+        <FontAwesomeIcon className="fa" icon={faLinkedinIn} color="#E07A5F" />
       </a>
-      <a class="btn github" href="#">
-        <FontAwesomeIcon className="fa" icon={faGithubAlt} />
+      <a class="btn github" href="https://github.com/sheldon-st">
+        <FontAwesomeIcon
+          className="fa githubIcon"
+          icon={faGithubAlt}
+          color="red"
+        />
       </a>
     </div>
   );
