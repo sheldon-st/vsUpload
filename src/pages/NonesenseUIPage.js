208d1f2522384e4e33ab1f6ef3cd644ade2a5755
sheldon-st <sheldon.st@northeastern.edu>
2022-10-17 16:19:12 -0400

    updated work sample

--- src/pages/NonesenseUIPage.js
+++ src/pages/NonesenseUIPage.js
@@ -10,6 +10,10 @@ import Featured from "../components/Featured";
 import ParagraphSectionWork from "../components/ParagraphSectionWork";
 import Hero from "../components/Hero";
 import useLocoScroll from "../hooks/useLocoScroll";
+import sketches from "../img/updatedFlowNoExtrasSVG.svg";
+import mock1 from "../img/Opt-In Form.png";
+import mock2 from "../img/Match Logic.png";
+import "../styles/main.css";
 
 const NonsenseUIPage = () => {
   const ref = useRef(null);
@@ -20,22 +24,65 @@ const NonsenseUIPage = () => {
         <ParagraphSectionWork
           content={
             <div id="headline" data-scroll-container ref={ref}>
-              <h1 id="headline">Nonsense UI</h1>
+              <h1 id="headline csH1" className="csH1">
+                Covalent - Web App UI
+              </h1>
               <ul id="headline">
-                <li>ARTG 3333 Experience & Interaction</li>
-                <li>Prototyping in Adobe XD</li>
-                <li>Development in HTML\CSS\Vanilla JS</li>
+                <li>- UX + UI Work Sample</li>
+                <li>- Prototyping in Figma</li>
               </ul>
 
               {/* <h2 id="headline">Prompt</h2> */}
 
-              <h2 id="Mock-Ups">
-                <a href="https://xd.adobe.com/view/d149dbbb-8b00-4c8d-accc-d6ceb9b28914-841a/">
-                  Prototypes
-                </a>
-              </h2>
+              <h2 id="Mock-Ups">Background</h2>
+              <p>
+                Covalent is a startup attempting to drive engagement for
+                communities and teams through custom email/slack based member
+                matching and introductions. When I joined the team; they had
+                developed an effective strategy for matching and introducing
+                members, but still had to onboard, configure and manage the
+                engagements manually. I worked with with the two founders and
+                another engineer to design and develop a web app that would
+                automate these processes. We divided the core functionality of
+                this into several projects following the agile methodology. This
+                is some of the work I did for the first of these projects:
+                "Match Logic."
+              </p>
 
-              <h2 id="Artifacts">Live Demo</h2>
+              <h2 id="Artifacts">Process + Artifacts</h2>
+              <h3 id="Artifacts">Project Spec</h3>
+              <h3 id="Artifacts">Ideation</h3>
+              <p id="Artifacts">
+                After finalizing the project spec with the rest of the team, we
+                began ideating on the design of the this feature; first
+                determining the restrictions/scope of the feature and possible
+                user flows. Another employee and I spent some time considering
+                the user flows, and revising this until we had a solid idea of
+                the experience we wanted to implement. From here, we began
+                sketching out the UI and iterating on the design. Here are some
+                fragments from this process:
+              </p>
+              <img class="csImage" src={sketches} alt="sketches" />
+              <h3 id="Artifacts">Prototyping</h3>
+              <p id="Artifacts">
+                After finalizing the design, we began prototyping in Figma.
+                We used the TailwindUI framework out design system
+                to ensure consistency across their product and speed up
+                development signifigantly. Using this system as a guide, we created high
+                fidelity mockups of the UI, and after a few rounds of internal
+                feedback; turned these into functional prototypes in Figma to
+                test with customers. Here are some of the final mockups for the Match Logic UI:
+              </p>
+              <img class="csImage" src={mock2} alt="mock2" />
+              <img class="csImage" src={mock1} alt="mock1" />
+              <h3 id="Artifacts">Development</h3>
+              <p id="Artifacts"> 
+                After finalizing the design in Figma, these mockups were passed to the senior front-end developer to implement in React. I worked with him to ensure that the UI was implemented as intended, and that the functionality was as expected. Sticking to the design system, we were able to achieve this relatively quickly. The final product was a fully functional UI that allowed users to configure and manage their match logic, and was later integrated into the rest of the product by the backend team.
+              </p>
+              <h3 >Challenges</h3>
+              <p> 
+                There were a few aspects of this project that presented challenges. The design decision we spent the most time evaluating was how matching criteria should be ranked/ ordered. Covalent's matching algorithm allows for a criteria to either be required or weighted, and we wanted to allow users to configure this in the UI. This was a bit of a challenge to implement, as we wanted to allow users to drag and drop the criteria to reorder them, but also wanted to allow them to toggle the required/weighted status of each criteria, and both needed to fit into a cohesive UI. We decided to implement this by allowing users to drag and drop the criteria to reorder them, and to toggle the required/weighted status by clicking on the criteria (which would remove it from the weighted section and indicate it is locked/required). This allowed us to keep the UI simple and intuitive, while still allowing users to configure the matching logic as they desired.
+              </p>
             </div>
           }
         />
